306c756f822cbc18e28bd5196d551bc7510b1677
Melanie Stoeckle <mpstoeckle@gmail.com>
2017-09-28 12:26:00 -0400

    almost done with the checkForWin function...

--- app/main.js
+++ app/main.js
@@ -19,7 +19,7 @@ function startGame() {
 	// remove previous winner notification and make sure div is hidden
 
 	// Clear board and add click events on squares
-	for (i = 0; i < spaces.length; i++) {
+	for (var i = 0; i < spaces.length; i++) {
 		spaces[i].innerHTML = ''; // Wipes out board when 'New Game' is clicked
 		spaces[i].addEventListener("click", takeSpace);
 	}
@@ -30,4 +30,17 @@ function takeSpace() {
 	this.innerHTML = symbols[turn % 2]; // The modulus swaps between 0 and 1 of the symbols array
 	this.removeEventListener("click", takeSpace); // `this` is always the object that you're acting on right now
 	// console.log(this); <-- Shows in the console the computer remembering which space you were on
+
+	for (var i = 0; i < wins.length; i++) {
+		if (checkForWin(wins[i])) {
+			alert("winner!");
+		}
+	}
+}
+
+function checkForWin(winArray) {
+		return
+			spaces[winArray[0]] !== '' && 
+			spaces[winArray[0]] === spaces[winArray[1]] && 
+			spaces[winArray[0]] === spaces[winArray[2]];
 }
