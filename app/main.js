9c72886d2cc18e0963b4ac399f961eb6ffff6b01
Melanie Stoeckle <mpstoeckle@gmail.com>
2017-09-28 11:32:50 -0400

    User can now click on spaces and record moves, New Game button clears board

---
+++ app/main.js
@@ -0,0 +1,33 @@
+// Global Variables
+var restartButton = document.getElementById('restartButton');
+var spaces = document.getElementsByClassName('space');
+var symbols = ['O', 'X'];
+var turn = 0;
+
+// Wait for page to load before doing anything
+document.onreadystatechange = function() {
+	if (document.readyState == "interactive") {
+		restartButton.onclick = startGame;
+		startGame();
+	}
+};
+
+function startGame() {
+	// reset turn counter
+	turn = 0;
+
+	// remove previous winner notification and make sure div is hidden
+
+	// Clear board and add click events on squares
+	for (i = 0; i < spaces.length; i++) {
+		spaces[i].innerHTML = ''; // Wipes out board when 'New Game' is clicked
+		spaces[i].addEventListener("click", takeSpace);
+	}
+}
+
+function takeSpace() {
+	turn++;
+	this.innerHTML = symbols[turn % 2]; // The modulus swaps between 0 and 1 of the symbols array
+	this.removeEventListener("click", takeSpace); // `this` is always the object that you're acting on right now
+	// console.log(this); <-- Shows in the console the computer remembering which space you were on
+}
