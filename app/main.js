4374ef0ebfb039eeca9dde01bcc60f5fc20599ef
Melanie Stoeckle <mpstoeckle@gmail.com>
2017-09-28 14:23:46 -0400

    Almost finished with tic tac toe

--- app/main.js
+++ app/main.js
@@ -3,6 +3,8 @@ var restartButton = document.getElementById('restartButton');
 var spaces = document.getElementsByClassName('space');
 var symbols = ['O', 'X'];
 var turn = 0;
+var notification = document.getElementById('notification');
+var winnerMessage = document.getElementById('winnerMessage');
 
 // Wait for page to load before doing anything
 document.onreadystatechange = function() {
@@ -17,6 +19,8 @@ function startGame() {
 	turn = 0;
 
 	// remove previous winner notification and make sure div is hidden
+	winnerMessage.innerHTML = '';
+	notification.style.display = 'none';
 
 	// Clear board and add click events on squares
 	for (var i = 0; i < spaces.length; i++) {
@@ -27,20 +31,34 @@ function startGame() {
 
 function takeSpace() {
 	turn++;
-	this.innerHTML = symbols[turn % 2]; // The modulus swaps between 0 and 1 of the symbols array
+	var currentPlayer = symbols[turn % 2];
+	this.innerHTML = currentPlayer; // The modulus swaps between 0 and 1 of the symbols array
 	this.removeEventListener("click", takeSpace); // `this` is always the object that you're acting on right now
 	// console.log(this); <-- Shows in the console the computer remembering which space you were on
 
 	for (var i = 0; i < wins.length; i++) {
 		if (checkForWin(wins[i])) {
-			alert("winner!");
+			
+			// No more clicking on the board
+			for (var j = 0; j < spaces.length; j++) {
+				spaces[j].removeEventListener("click", takeSpace);
+			}
+
+			// Notify the players
+			notification.style.display = 'block';
+			winnerMessage.innerHTML = currentPlayer + " won!";
+		} else {
+			
+			if (turn == 9) {
+				notification.style.display = 'block';
+				winnerMessage.innerHTML += "Tie game";
+			}
 		}
 	}
 }
 
 function checkForWin(winArray) {
-		return
-			spaces[winArray[0]] !== '' && 
-			spaces[winArray[0]] === spaces[winArray[1]] && 
-			spaces[winArray[0]] === spaces[winArray[2]];
+		return spaces[winArray[0]].innerHTML !== '' && 
+			spaces[winArray[0]].innerHTML === spaces[winArray[1]].innerHTML && 
+			spaces[winArray[0]].innerHTML === spaces[winArray[2]].innerHTML;
 }
